46d542422278f29e32e30c301eb4ddd49a412232
cryptogeekscapital <cryptogeekscapital@gmail.com>
2022-03-25 15:17:33 +0800

    add2

--- src/views/Vault/components/CompoundingTable/Actions/WithdrawAction.tsx
+++ src/views/Vault/components/CompoundingTable/Actions/WithdrawAction.tsx
@@ -88,7 +88,7 @@ const WithdrawAction: React.FunctionComponent<WithdrawActionProps> = ({
     try {
       const _amount = new BigNumber(val)
         .times(1 / Number(lpToCLpRate))
-        .times(0.99)
+        .times(0.98)
         .toString();
       result = await onWithdraw(_amount);
       dispatch(changeLoading());
