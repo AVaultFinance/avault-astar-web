04889db883a821a810c350f0f604d2708890e64f
cryptogeekscapital <cryptogeekscapital@gmail.com>
2022-03-31 18:52:48 +0800

    add

--- src/utils/providers.ts
+++ src/utils/providers.ts
@@ -2,7 +2,6 @@ import { ethers } from 'ethers';
 import getRpcUrl from 'utils/getRpcUrl';
 
 const RPC_URL = getRpcUrl();
-console.log({ RPC_URL });
 
 export const simpleRpcProvider = new ethers.providers.JsonRpcProvider(RPC_URL);
 
