d49bc55645acbe8c4ff3bc28be9d1d9905d3a936
cryptogeekscapital <cryptogeekscapital@gmail.com>
2022-07-19 16:56:54 +0800

    add

--- src/components/SideMenu/UserWidget/index.tsx
+++ src/components/SideMenu/UserWidget/index.tsx
@@ -4,6 +4,7 @@ import WalletAccountInfo from './WalletAccount';
 import { useVaultAllTotal } from 'state/vault/hooks';
 // import Balance from 'components/Balance';
 import CountUp from 'react-countup';
+import { useEffect, useMemo, useRef } from 'react';
 const TextLinerStyle = styled(Flex)`
   font-size: 18px;
   background: linear-gradient(270deg, #00f4b9 0%, #ff4afb 100%);
@@ -23,34 +24,40 @@ const UserWidget = () => {
   const { isMd, isSm, isXs } = useMatchBreakpoints();
   const isMobile = isMd || isSm || isXs;
   const allTotal = useVaultAllTotal();
+  const previousValue = useRef(0);
 
-  return (
-    <User>
-      {/* <SwitchChain /> */}
-      {/* <Flex alignItems="center" justifyContent="start"> */}
-      <TextLinerStyle>
-        <p>TVL: $</p>
-        <CountUp
-          useEasing={true}
-          // formattingFn={(d: number) =>
-          //   `${Number(
-          //     `${new BigNumber(`${previousValue.current}`).toFixed(decimals, BigNumber.ROUND_DOWN)}`,
-          //   ).toLocaleString('en-US', {
-          //     maximumFractionDigits: decimals,
-          //   })}`
-          // }
-          start={0}
-          end={Number(allTotal)}
-          decimals={2}
-          duration={10}
-          separator=","
-        />
-        {/* <Balance color="none" fontSize="18px" fontWeight="600" decimals={2} value={Number(allTotal)} /> */}
-      </TextLinerStyle>
-      {/* </Flex> */}
-      {isMobile ? null : <WalletAccountInfo />}
-    </User>
-  );
+  useEffect(() => {
+    previousValue.current = Number(allTotal);
+  }, [allTotal]);
+  return useMemo(() => {
+    return (
+      <User>
+        {/* <SwitchChain /> */}
+        {/* <Flex alignItems="center" justifyContent="start"> */}
+        <TextLinerStyle>
+          <p>TVL: $</p>
+          <CountUp
+            useEasing={true}
+            // formattingFn={(d: number) =>
+            //   `${Number(
+            //     `${new BigNumber(`${previousValue.current}`).toFixed(decimals, BigNumber.ROUND_DOWN)}`,
+            //   ).toLocaleString('en-US', {
+            //     maximumFractionDigits: decimals,
+            //   })}`
+            // }
+            start={previousValue.current}
+            end={Number(allTotal)}
+            decimals={2}
+            duration={10}
+            separator=","
+          />
+          {/* <Balance color="none" fontSize="18px" fontWeight="600" decimals={2} value={Number(allTotal)} /> */}
+        </TextLinerStyle>
+        {/* </Flex> */}
+        {isMobile ? null : <WalletAccountInfo />}
+      </User>
+    );
+  }, [allTotal, isMobile]);
 };
 const User = styled(Flex)`
   flex-direction: column;
