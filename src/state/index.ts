08559ce2a3563fa184034c993fd333975ea0c966
cryptogeekscapital <cryptogeekscapital@gmail.com>
2022-08-08 18:10:07 +0800

    add

--- src/state/index.ts
+++ src/state/index.ts
@@ -76,8 +76,8 @@ const store = configureStore({
   },
   // middleware: [...getDefaultMiddleware({ thunk: true }), save({ states: PERSISTED_KEYS, namespace: 'v1' })],
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({ thunk: true }).concat(save({ states: PERSISTED_KEYS, namespace: 'v5' })),
-  preloadedState: load({ states: PERSISTED_KEYS, namespace: 'v5' }),
+    getDefaultMiddleware({ thunk: true }).concat(save({ states: PERSISTED_KEYS, namespace: 'v9' })),
+  preloadedState: load({ states: PERSISTED_KEYS, namespace: 'v9' }),
 });
 
 store.dispatch(updateVersion());
