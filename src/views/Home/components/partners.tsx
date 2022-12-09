6debd39c3e3e051ea291c25c399aa1d6c80416c5
cryptogeekscapital <cryptogeekscapital@gmail.com>
2022-12-09 14:06:47 +0800

    update partner

--- src/views/Home/components/partners.tsx
+++ src/views/Home/components/partners.tsx
@@ -1,325 +1,199 @@
-import { Flex } from '@my/ui';
 import { STATIC_BASE_URL } from 'config';
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 
 export const partnersArr = [
   {
     icon: 'Astar.svg',
-    name: 'Astar',
+    name: 'astar'.toUpperCase(),
     link: 'https://astar.network/',
   },
   {
     icon: 'Next Web.svg',
-    name: 'Next Web',
+    name: 'Next Web'.toUpperCase(),
     link: 'https://nextweb.capital/',
   },
-
-  {
-    icon: 'AU21.svg',
-    name: 'AU21',
-    link: 'https://au21.capital/',
-  },
-
   {
     icon: 'Cogitent.svg',
-    name: 'Cogitent',
+    name: 'Cogitent'.toUpperCase(),
     link: 'https://cogitent.ventures/',
   },
   {
-    icon: 'Astriddao.svg',
-    name: 'Astriddao',
-    link: 'https://astriddao.xyz/',
-  },
-  {
-    icon: 'Sirius.svg',
-    name: 'Sirius',
-    link: 'https://www.sirius.finance/',
-  },
-];
-const arr02 = [
-  {
-    icon: 'Starbank.svg',
-    name: 'Starbank',
-    link: 'https://test.starbank.finance/#/',
-  },
-  {
-    icon: 'Libra.svg',
-    name: 'Libra',
-    link: 'https://drive.google.com/drive/folders/1Ydb2tV67qafHo3Er6fitaUmk9KihPg0v',
-  },
-
-  {
-    icon: 'Astar Sign Witch.svg',
-    name: 'Astar Sign Witch',
-    link: 'https://sign-witch.vercel.app/',
-  },
-  {
-    icon: 'Peckshield.svg',
-    name: 'Peckshield',
-    link: 'https://peckshield.com/',
-  },
-  {
-    icon: 'Astar Domain.svg',
-    name: 'Astar Domain',
-    link: 'https://astr.domains/',
-  },
-  {
     icon: 'GTS VENTURES.svg',
-    name: 'GTS VENTURES',
+    name: 'GTS VENTURES'.toUpperCase(),
     link: 'https://gts.ventures/',
   },
   {
     icon: 'Microsoft.svg',
-    name: 'Microsoft',
+    name: 'Microsoft'.toUpperCase(),
     link: '',
+  },
+  {
+    icon: 'Stargate_Logo_Full.svg',
+    name: 'Stargate'.toUpperCase(),
+    link: 'https://stargate.finance/',
+  },
+  {
+    icon: 'Peckshield.svg',
+    name: 'Peckshield'.toUpperCase(),
+    link: 'https://peckshield.com/',
+  },
+  // {
+  //   icon: 'Bwarelabs.svg',
+  //   name: 'BwareLabs'.toUpperCase(),
+  //   link: 'https://bwarelabs.com/',
+  // },
+  {
+    icon: 'Arthswap_wh.svg',
+    name: 'Arthswap'.toUpperCase(),
+    link: 'https://app.arthswap.org/#/swap',
+  },
+  {
+    icon: 'Unstoppabledomain.svg',
+    name: 'Unstoppabledomain'.toUpperCase(),
+    link: 'https://unstoppabledomains.com/',
+  },
+  {
+    icon: 'Talisman.svg',
+    name: 'Talisman'.toUpperCase(),
+    link: 'https://talisman.xyz/',
+  },
+
+  // Arthswap: https://app.arthswap.org/#/swap
+  // BwareLabs: https://bwarelabs.com/
+  // Stargate: https://stargate.finance/
+  // Unstoppabledomain: https://unstoppabledomains.com/
+  // Talisman: https://talisman.xyz/
+
+  {
+    icon: `Sirius.svg`,
+    name: 'Sirius'.toUpperCase(),
+    link: 'https://www.sirius.finance/',
+  },
+
+  {
+    icon: `Astriddao.svg`,
+    name: 'Astriddao'.toUpperCase(),
+    link: 'https://astriddao.xyz/',
   },
 ];
 
-const marquee = keyframes`
-0% {
-  transform: translateY(0);
-}
-100% {
-  transform: translateY(-100%);
-}
-`;
 const HomePartners = () => {
   return (
-    <HomePartnersStyled>
-      <h3>Partners</h3>
-      <div className="ul_wrap_wrap">
-        <div className="ul_wrap">
-          {/* <div className="ul_wrap animate animate__animated" data-animate="animate__show"> */}
-          <ul>
-            {partnersArr.map((v, index) => {
-              return (
-                <li key={index}>
-                  <a href={v.link} target="_blank" rel="noreferrer" title={v.name}>
-                    <img src={`${STATIC_BASE_URL}/images/home/partner_on/${v.icon}`} alt={v.name} />
-                  </a>
-                </li>
-              );
-            })}
-            {partnersArr.length % 2 !== 0 ? <li className="null"></li> : null}
-          </ul>
-          <ul>
-            {partnersArr.map((v, index) => {
-              return (
-                <li key={index}>
-                  {v.link === '' ? (
-                    <img src={`${STATIC_BASE_URL}/images/home/partner_on/${v.icon}`} alt={v.name} />
-                  ) : (
-                    <a href={v.link} target="_blank" rel="noreferrer" title={v.name}>
-                      <img src={`${STATIC_BASE_URL}/images/home/partner_on/${v.icon}`} alt={v.name} />
-                    </a>
-                  )}
-
-                  {/* <IconBg name={v.name} href={v.link} target="_blank" rel="noreferrer" title={v.name} /> */}
-                </li>
-              );
-            })}
-            {partnersArr.length % 2 !== 0 ? <li className="null"></li> : null}
-          </ul>
-        </div>
-        <div className="ul_wrap two">
-          {/* <div className="ul_wrap animate animate__animated" data-animate="animate__show"> */}
-          <ul>
-            {arr02.map((v, index) => {
-              return (
-                <li key={index}>
-                  <a href={v.link} target="_blank" rel="noreferrer" title={v.name}>
-                    <img src={`${STATIC_BASE_URL}/images/home/partner_on/${v.icon}`} alt={v.name} />
-                  </a>
-                </li>
-              );
-            })}
-            {partnersArr.length % 2 !== 0 ? <li className="null"></li> : null}
-          </ul>
-          <ul>
-            {arr02.map((v, index) => {
-              return (
-                <li key={index}>
-                  <a href={v.link} target="_blank" rel="noreferrer" title={v.name}>
-                    <img src={`${STATIC_BASE_URL}/images/home/partner_on/${v.icon}`} alt={v.name} />
-                  </a>
-                  {/* <IconBg name={v.name} href={v.link} target="_blank" rel="noreferrer" title={v.name} /> */}
-                </li>
-              );
-            })}
-            {partnersArr.length % 2 !== 0 ? <li className="null"></li> : null}
-          </ul>
-        </div>
-      </div>
+    <HomePartnersStyled className="partner animate animate__animated" data-animate="animate__show">
       <h2>Partners</h2>
+      <ul>
+        {partnersArr.map((v, index) => {
+          return (
+            <LiStyled key={index} index={index} className={v.name === 'BwareLabs'.toUpperCase() ? 'BwareLabs' : ''}>
+              <a href={v.link} target="_blank" rel="noreferrer" title={v.name}>
+                <img src={`${STATIC_BASE_URL}/images/home/partner_on/${v.icon}`} alt={v.name} />
+              </a>
+            </LiStyled>
+          );
+        })}
+        <li className="null"></li>
+        <li className="null"></li>
+      </ul>
     </HomePartnersStyled>
   );
 };
-const HomePartnersStyled = styled(Flex)`
-  display: block;
+const LiStyled = styled.li<{ index: number }>`
+  animation-delay: ${({ index }) => 0.2 * index}s;
+  &.BwareLabs {
+    padding: 2.5%;
+  }
+`;
+const HomePartnersStyled = styled.div`
   padding: 0 20px;
-
   ${({ theme }) => theme.mediaQueries.xl} {
-    display: flex;
     max-width: 1208px;
     margin: 0 auto;
-    justify-content: space-between;
-    align-items: center;
   }
   ${({ theme }) => theme.mediaQueries.xl} {
-    padding: 0;
+    padding: 20px 0 100px;
   }
-  .animate__show {
+  &.animate__show {
     li {
-      animation: slide-up 1s ease-in-out;
+      animation-duration: 1s;
+      animation-timing-function: ease-in-out;
+      animation-iteration-count: 1;
+      animation-direction: normal;
+      animation-fill-mode: forwards;
+      animation-play-state: running;
+      animation-name: slide-up;
       animation-fill-mode: forwards;
     }
   }
-  h3 {
-    font-size: 48px;
-    text-align: center;
-    padding-top: 60px;
-    display: block;
-    background: linear-gradient(90deg, #ffd8fe 0%, #c5fff1 100%);
-    -webkit-background-clip: text;
-    color: transparent;
-    ${({ theme }) => theme.mediaQueries.xl} {
-      display: none;
-    }
-  }
+
   h2 {
-    font-size: 80px;
-    display: none;
-    background: linear-gradient(90deg, #ffd8fe 0%, #c5fff1 100%);
-    -webkit-background-clip: text;
-    color: transparent;
-    ${({ theme }) => theme.mediaQueries.xl} {
-      display: block;
+    padding: 60px 20px;
+    text-align: center;
+    font-size: 40px;
+    font-weight: 700;
+    ${({ theme }) => theme.mediaQueries.md} {
+      padding: 160px 120px;
+      font-size: 80px;
     }
   }
-  .ul_wrap_wrap {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    ${({ theme }) => theme.mediaQueries.xl} {
-      width: 700px;
-      margin: 200px 0 170px;
-    }
-    &:hover {
-      ul {
-        animation-play-state: paused;
-      }
-    }
-  }
-  .ul_wrap {
-    margin: 60px 0;
-    height: 302px;
-    overflow: hidden;
-    width: 50%;
-    ${({ theme }) => theme.mediaQueries.sm} {
-      height: 482px;
-    }
-    &.two {
-      ul {
-        // animation-delay: 0.3s;
-      }
-    }
-  }
+
   ul {
-    animation: ${marquee} 15s linear infinite;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    li {
+      opacity: 0;
+      width: 40%;
+      height: 60px;
+      border: 1px solid #2e2d50;
+      border-radius: 12px;
+      display: inline-block;
+      margin-bottom: 16px;
+      background-position: 0 -60px;
+      background-color: #161528;
+      background-image: radial-gradient(circle at 50% 0%, #3e255b 0%, #161528 100%);
+      transition: background-position 0.5s ease;
+      background-repeat: no-repeat;
+      margin-right: 8px;
+      margin-left: 8px;
+      &.null {
+        background: none;
+        border: none;
+        &:hover {
+          background: none;
+          border: none;
+        }
+      }
+      ${({ theme }) => theme.mediaQueries.sm} {
+        background-position: 0 -80px;
+        height: 80px;
+      }
+      ${({ theme }) => theme.mediaQueries.md} {
+        width: 30%;
+        background-position: 0 -100px;
+        height: 100px;
+      }
+      ${({ theme }) => theme.mediaQueries.xl} {
+        width: 320px;
+        height: 120px;
+        margin-right: 15px;
+        margin-left: 15px;
+        margin-bottom: 30px;
+        border-radius: 20px;
+        background-position: 0 -120px;
+      }
 
-    // backface-visibility: hidden;
-    // -webkit-transform: translate3d(0, 0, 0);
-    // -webkit-backface-visibility: hidden;
-    // -webkit-transform-style: preserve-3d;
-    a {
-      diplay: block;
-      opacity: 0.7;
-      transition: opacity 0.3s ease;
       &:hover {
-        opacity: 1;
+        background-position: 0 0;
+      }
+
+      img {
+        height: 100%;
+        margin: 0 auto;
+        display: block;
       }
     }
   }
-  // ul {
-  //   clear: both;
-  //   overflow: hidden;
-  //   text-align: center;
-  //   transform-origin: 0% 0%;
-
-  //   animation: ${marquee} 20s linear infinite;
-  //   animation-fill-mode: forwards;
-  //   backface-visibility: hidden;
-  //   -webkit-transform: translate3d(0, 0, 0);
-  //   -webkit-backface-visibility: hidden;
-  //   -webkit-transform-style: preserve-3d;
-  //   animation-delay: 0s;
-  //   ${({ theme }) => theme.mediaQueries.sm} {
-  //   }
-
-  //   li {
-  //     // opacity: 0;
-  //     width: 200px;
-  //     height: 60px;
-  //     border: 1px solid #2e2d5b;
-  //     border-radius: 12px;
-  //     display: inline-block;
-  //     margin-bottom: 15px;
-  //     // background-image: radial-gradient(circle at 50% 0%, #181733 0%, #181733 100%);
-  //     background-position: 0 -60px;
-  //     background-color: #181733;
-  //     background-image: radial-gradient(circle at 50% 0%, #3e255b 0%, #040222 100%);
-  //     transition: background-position 0.5s ease;
-  //     background-repeat: no-repeat;
-  //     &.null {
-  //       background: none;
-  //       border: none;
-  //       &:hover {
-  //         background: none;
-  //         border: none;
-  //       }
-  //     }
-  //     &:nth-child(2n + 1) {
-  //       margin-right: 15px;
-  //     }
-  //     ${({ theme }) => theme.mediaQueries.sm} {
-  //       margin-right: 15px;
-  //       background-position: 0 -80px;
-  //       height: 80px;
-  //     }
-  //     ${({ theme }) => theme.mediaQueries.md} {
-  //       background-position: 0 -100px;
-  //       height: 100px;
-  //     }
-  //     ${({ theme }) => theme.mediaQueries.xl} {
-  //       float: left;
-  //       width: 320px;
-  //       height: 120px;
-  //       margin-right: 30px;
-  //       margin-bottom: 30px;
-  //       border-radius: 20px;
-  //       background-position: 0 -120px;
-  //       &:nth-child(2n + 1) {
-  //         margin-right: 30px;
-  //       }
-  //     }
-
-  //     &:hover {
-  //       background-position: 0 0;
-  //     }
-  //     // a {
-  //     //   display: block;
-  //     //   width: 100%;
-  //     //   height: 100%;
-  //     //   background-repeat: no-repeat;
-  //     //   background-size: 80%;
-  //     //   background-position: center;
-  //     // }
-  //     img {
-  //       height: 100%;
-  //       margin: 0 auto;
-  //       display: block;
-  //     }
-  //   }
-  // }
 `;
 export default HomePartners;
